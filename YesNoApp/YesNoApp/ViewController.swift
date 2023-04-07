96b6fe213903c5f33042ccd4a1a6963f334610dc
melanna012 <anna.swift0012@gmail.com>
2023-04-07 05:39:56 +0300

    Lesson code

--- YesNoApp/YesNoApp/ViewController.swift
+++ YesNoApp/YesNoApp/ViewController.swift
@@ -2,18 +2,27 @@
 //  ViewController.swift
 //  YesNoApp
 //
-//  Created by Anna on 07.04.2023.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var answerButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+//  answerButton.layer.cornerRadius = 5
     }
 
+    @IBAction func answerButtonAction(_ sender: Any) {
+        let answer = Bool.random()
+        answerLabel.text = ""
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.answerLabel.text = answer ? "YES" : "NO"
+        }
+    }
 
 }
 
